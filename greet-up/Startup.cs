4f99440bbfc4a74e5fd4fedfc1eb76ff64c9029e
Malyssa Winters <malyssawinters5@gmail.com>
2016-07-19 14:34:11 -0400

    set up model

---
+++ greet-up/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(greet_up.Startup))]
+namespace greet_up
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
